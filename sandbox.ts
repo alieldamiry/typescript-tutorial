8a0fed70a85edad646a623e92d8909946b0a7bac
Ali <alieldamiry@gmail.com>
2020-09-03 15:03:35 +0200

    L3

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,17 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
 // character = 200;
 character = "loigie";
+
 // age ='ali';
 age = 10;
 // isBlackBelt = 'yes'
 isBlackBelt = true;
-var circ = function (diameter) {
+
+const circ = (diameter:number) => {
     return diameter * Math.PI;
-};
+}
+
 console.log(circ(7.5));
